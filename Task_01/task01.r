fda43b32517e6cc4d260b2c8e56a881cb22827c8
sbd98 <59843705+sbd98@users.noreply.github.com>
2020-01-15 13:21:57 -0500

    Sarah Daniel Task 01 upload b

---
+++ Task_01/task01.r
@@ -0,0 +1,20 @@
+library(swirl)
+swirl()
+Sarah Daniel
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2+100
+my_sqrt <- sqrt(z-1)
+c(0.10, 8, 2.14)
+my_sqrt
+my_div <- z/my_sqrt
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my
