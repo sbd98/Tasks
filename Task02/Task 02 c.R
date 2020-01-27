2b9a0f237a88c4bef539e221551e7dcc623a9407
sbd98 <59843705+sbd98@users.noreply.github.com>
2020-01-27 09:33:20 -0500

    Sarah Daniel Task 02 upload c

--- Task02/Task 02 c.R
+++ Task02/Task 02 c.R
@@ -1,11 +1,15 @@
-# The rate at which Beren grows is speeding up over time
+# Hypothesis: The rate at which Beren grows is speeding up over time
 
 setwd("~/Desktop/Evolution/Tasks/Task02")
 beren4 <- read.csv("beren_new.csv", stringsAsFactors=F)
+head(beren4)
+Weight <- which(beren4$event == "trait_mass")
 
-Growth <- which(beren4$value == "trait_mass")
-
-
+Weight[1:4]
 par(las=1, mar=c(5, 5, 1, 1), mgp=c(2, 0.5, 0), tck =-0.01)
-plot(as.numeric(names(beren4$value[Growth] ~ beren4$age[Growth])), type="b", pch=16, xlab="age in days", ylab="mass in kg", xlim=c(0, 218), ylim=c(4, 10))
+plot(beren4$age[Weight], beren4$value[Weight], type="p", xlab="age in days", ylab="mass in kg")
 
+pdf("BerenGrowthOverTime.pdf")
+par(las=1, mar=c(5, 5, 1, 1), mgp=c(2, 0.5, 0), tck =-0.01)
+plot(beren4$age[Weight], beren4$value[Weight], type="p", xlab="age in days", ylab="mass in kg")
+dev.off()
