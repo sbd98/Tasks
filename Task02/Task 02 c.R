7b0aea09f10a8a251ae758ca663db0b5208ac5e7
sbd98 <59843705+sbd98@users.noreply.github.com>
2020-01-27 09:58:53 -0500

    Sarah Daniel Task 02 upload c

--- Task02/Task 02 c.R
+++ Task02/Task 02 c.R
@@ -13,3 +13,4 @@ pdf("BerenGrowthOverTime.pdf")
 par(las=1, mar=c(5, 5, 1, 1), mgp=c(2, 0.5, 0), tck =-0.01)
 plot(beren4$age[Weight], beren4$value[Weight], type="p", xlab="age in days", ylab="mass in kg")
 dev.off()
+
