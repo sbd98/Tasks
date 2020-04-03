953b40c6bb8da4e559e1edd4124e6b458734a0a8
sbd98 <59843705+sbd98@users.noreply.github.com>
2020-04-03 01:14:27 -0400

    Sarah Daniel Project plot

--- Project/code/Project.R
+++ Project/code/Project.R
@@ -11,6 +11,10 @@ my_data$Capture.TumourLoad
 na.omit(my_data)
 plot(my_data$Capture.Date, my_data$Capture.TumourLoad, xlab="Year", ylab="Tumor Load", type="p", main="Tumor Load of DFTD Among Tasmanian Devils is Decreasing Over Time", pch=19)
 
+pdf("DFTD.pdf", height = 6, width = 8)
+plot(my_data$Capture.Date, my_data$Capture.TumourLoad, xlab="Year", ylab="Tumor Load", type="p", main="Tumor Load of DFTD Among Tasmanian Devils is Decreasing Over Time", pch=19)
+dev.off()
+
 # What I've done: Collected data over 10 years on Tasmanian devils & created a plot to show a decreased load/occurrence of DFTD over time.
 #Analysis Plan: What I want to do: Show decreasing load/occurrence of DFTD among Tasmanian devils, maybe expain why/how this is happening (resistance? If so, how?) 
 #What I still have left to do: find more studies on DFTD 
