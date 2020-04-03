6c9cd0311ee8f2bed1134a9890e2fbe8be151603
sbd98 <59843705+sbd98@users.noreply.github.com>
2020-04-02 22:56:08 -0400

    Sarah Daniel Task 09

--- Project/code/Project.R
+++ Project/code/Project.R
@@ -1,11 +1,17 @@
-setwd("~/Desktop/Evolution/Tasks/Project")
-#I hypothesize that the higher "fit" tasmanian devils (higher number of offspring, longer survival, socially dominant) are becoming less suceptible to infection with devil facial tumor disease over time (DFTD).
-#Tasmanian devils with higher fitness are more likely to be infected with the disease but are forming resistance
+setwd("~/Desktop/Evolution/Tasks/Project/data")
+
 install.packages("readxl")
-library("readxl")
-my_data <- read_excel("TasDevil.xlsx")
+library(readxl)
+read_excel("TasDevil.xlsx")
 head(my_data)
 my_data <- read_excel("TasDevil.xlsx", na = "NA")
 
-#I will be adding more data but I can't get the file to unzip 
+my_data$Capture.Date
+my_data$Capture.TumourLoad
+na.omit(my_data)
+plot(my_data$Capture.Date, my_data$Capture.TumourLoad, xlab="Capture Date", ylab="Tumor Load", type="p", main="Tumor Load of DFTD Among Tasmanian Devils is Decreasing Over Time")
+
+# What I've done: Collected data over 10 years on Tasmanian devils & created a plot to show a decreased load/occurrence of DFTD over time.
+#Analysis Plan: What I want to do: Show decreasing load/occurrence of DFTD among Tasmanian devils, maybe expain why/how this is happening (resistance? If so, how?) 
+#What I still have left to do: find more studies on DFTD 
 
---
+++ Task09/Task09.R
@@ -0,0 +1,18 @@
+setwd("~/Desktop/Evolution/Tasks/Task09")
+install.packages("phytools")
+library("phytools")
+tree <- force.ultrametric(read.tree("https://jonsmitchell.com/data/anolis.tre"))
+plot(tree, type="fan")
+str(tree)
+
+compute.brlen(tree)
+# 1. 82 tips and branch lengths are present
+
+data <- read.csv("https://jonsmitchell.com/data/svl.csv", stringsAsFactors=F, row.names=1)
+head(data)
+class(data)
+dim(data)
+#2. "data" is a data frame. It has 100 rows and 1 column. 
+
+svl <- setNames(data$svl, rownames(data))
+Ancestors <- fastAnc(tree, svl[trees$tip.label], vars=TRUE, CI=TRUE)
