fa23bbf8c133be6a0e521f8ee3762c1975cafc15
sbd98 <59843705+sbd98@users.noreply.github.com>
2020-04-30 22:48:01 -0400

    Sarah Daniel Final Project upload

--- Project/code/Project.R
+++ Project/code/Project.R
@@ -6,13 +6,14 @@ read_excel("TasDevil.xlsx")
 head(my_data)
 my_data <- read_excel("TasDevil.xlsx", na = "NA")
 
+nrow(na.omit(my_data))
 my_data$Capture.Date
 my_data$Capture.TumourLoad
 na.omit(my_data)
-plot(my_data$Capture.Date, my_data$Capture.TumourLoad, xlab="Year", ylab="Tumor Load", type="p", main="Tumor Load of DFTD Among Tasmanian Devils is Decreasing Over Time", pch=19)
+plot(my_data$Capture.Date, my_data$Capture.TumourLoad, xlab="Year", ylab="Tumor Load", type="p", main="Tumor Load of DFTD Among Tasmanian Devils is Decreasing Over Time", pch=19, log="y")
 
 pdf("DFTD.pdf", height = 6, width = 8)
-plot(my_data$Capture.Date, my_data$Capture.TumourLoad, xlab="Year", ylab="Tumor Load", type="p", main="Tumor Load of DFTD Among Tasmanian Devils is Decreasing Over Time", pch=19)
+plot(my_data$Capture.Date, my_data$Capture.TumourLoad, xlab="Year", ylab="Tumor Load", type="p", main="Tumor Load of DFTD Among Tasmanian Devils is Decreasing Over Time", pch=19, log="y")
 dev.off()
 
 # What I've done: Collected data over 10 years on Tasmanian devils & created a plot to show a decreased load/occurrence of DFTD over time.
@@ -23,4 +24,7 @@ read_excel("DFTD samples 2006-2011:Anthropogenic .xls")
 my_data2 <- read_excel("DFTD samples 2006-2011:Anthropogenic .xls")
 
 
-plot(my_data2$year2, my_data2$Tetraploidy2, type="h", xlab="Year", ylab="Tetraploid Samples", main="Tetraploid Tumor Prevalence in Devils")
+plot(my_data2$year2, my_data2$Tetraploidy2, type="h", xlab="Year", ylab="Tetraploid Samples", main="Tetraploid Tumor Prevalence in Devils", lwd=6)
+pdf("Tetraploid.pdf", height=6, width=8)
+plot(my_data2$year2, my_data2$Tetraploidy2, type="h", xlab="Year", ylab="Tetraploid Samples", main="Tetraploid Tumor Prevalence in Devils", lwd=6)
+dev.off()
